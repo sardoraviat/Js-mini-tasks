a03735cd80ecb2ca5fb3374b5b5e0db9d92ff9cb
Sardor Narzullayev <sardor_aviat96@mail.ru>
2022-06-30 14:51:40 +0500

    sardor qosh

---
+++ index.js
@@ -0,0 +1,41 @@
+console.log(4 * 4)
+console.log("Hello JS!")
+console.log("San'at")
+console.log("Hello World!")
+var user = "Akmal Karimov"
+/*user = "Akmal Valiyev"*/
+var userAge = 22
+console.log('2b1' * 2)
+/* Buni natijasi NaN( Not a number ) ya'ni raqam emas */
+console.log(user)
+var a, a1, a2
+a = 15
+a1 = 'Height'
+a2 = 25
+
+console.log(a, a1, a2)
+console.log(user + ' ' + userAge + ' yoshlik talaba')
+var num1, num2, num3, num4, num5, num6
+num1 = 8;
+num2 = 5;
+num3 = 3;
+num4 = 6;
+num5 = 3;
+num6 = 3;
+console.log(num1 + 'x' + num2 + '=' + num1 * num2)
+console.log(num3 + '+' + num4 + '=' + (num3 + num4))
+console.log(num5 + 'x' + num5 + '=' + (num5 * num6 +1))
+var cash = 310000
+
+var car = {
+    brand: "Ferrari",
+    price: 300000,
+    isNew: false
+    /*Boolean (mantiqiy rost yoki yolg'on ma'lumot tipi (tipi))*/
+}
+if (cash >= car.price) {
+    console.log(car.brand + ' ni sotib ilishingiz mumkin')
+} else {
+    console.log(car.brand + ' ni sotib ololmisiz!')
+}
+console.log(car.brand + ' ' + '$' + car.price + ' lik'+ ' avtomobil')
